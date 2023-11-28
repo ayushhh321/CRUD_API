e28e5ff746d4e2d6b0fd7c58ab05b69cfb26d24c
ayushhh321 <ayush56039@gmail.com>
2023-11-28 17:20:45 +0530

    express app code doone

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 //added server.js in package.json so use npm run serve
+
+const express=require('express')
+const app = express()
+
+//routes- we have to declare routes so that web browser access the our web page
+
+
+
+app.listen(3000, ()=> {
+  console.log('App is running on port 3000 <ayush bhai>')
+})
