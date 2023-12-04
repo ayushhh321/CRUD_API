fb3cad46f4032b6576d3808c2cb68ca5ed996b89
ayushhh321 <ayush56039@gmail.com>
2023-12-05 04:54:15 +0530

    pused until labourperfloor

--- models/productModels.js
+++ models/productModels.js
@@ -2,8 +2,33 @@ const mongoose = require('mongoose')
 
 const productSchema = mongoose.Schema(//for product model we need schema so here we added it
     {
-        ProductName: {
+        group: {
             type: String,
+            required: [true, "Please enter a group"],//validation method
+        },
+
+        subgroup: {
+            type: String,
+            required: [true, "Please enter a subgroup"],//validation method
+        },
+
+        brand: {
+            type: String,
+            required: [true, "Please enter a brand"],//validation method
+        },
+
+        product_id: {
+            type: String,
+            required: [true, "Please enter a product id"],//validation method
+        },
+
+        plyUnit: {
+            type: Number,
+            required: [true, "Please enter a Plyunit"],//validation method
+        },
+
+        labourPerFloor: {
+            type: Number,
             required: [true, "Please enter a product name"],//validation method
         },
         ProductDescription: {
